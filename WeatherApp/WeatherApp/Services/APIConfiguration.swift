0bb67eb875422b3fe0b4b3d9256c6295d7adc2d8
Eric Palma <palmae95@gmail.com>
2023-12-12 08:30:19 -0500

    mmove code

--- WeatherApp/WeatherApp/Services/APIConfiguration.swift
+++ WeatherApp/WeatherApp/Services/APIConfiguration.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+
+enum APIError: Error {
+    case unableToBuildRequest
+}
+
+struct APIConfiguration {
+    let apiKey: String
+    let baseURL: String
+    let scheme: String = "https"
+    
+    var headers: [String: String]? {
+        var headers = [String: String]()
+        headers["Content-Type"] = "application/json"
+        
+        return headers
+    }
+    
+    init(apiKey: String, baseURL: String) {
+        self.apiKey = apiKey
+        self.baseURL = baseURL
+    }
+    
+}
--- WeatherApp/WeatherApp/Services/WeatherAPI.swift
+++ WeatherApp/WeatherApp/Services/WeatherAPI.swift
@@ -7,29 +7,6 @@
 
 import Foundation
 
-enum APIError: Error {
-    case unableToBuildRequest
-}
-
-struct APIConfiguration {
-    let apiKey: String
-    let baseURL: String
-    let scheme: String = "https"
-    
-    var headers: [String: String]? {
-        var headers = [String: String]()
-        headers["Content-Type"] = "application/json"
-        
-        return headers
-    }
-    
-    init(apiKey: String, baseURL: String) {
-        self.apiKey = apiKey
-        self.baseURL = baseURL
-    }
-    
-}
-
 struct WeatherAPI {
     enum APIEndpointPath {
         case weatherByCoordinates(lat: String, lon: String)
