8fa7194c2621646a8044d9d5b087aeba7efda3b0
Eric Palma <palmae95@gmail.com>
2023-12-11 20:09:29 -0500

    add bare bones of view model

--- WeatherApp/WeatherApp/WeatherViewModel.swift
+++ WeatherApp/WeatherApp/WeatherViewModel.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+
+class WeatherViewModel {
+
+    // This function fetches weather data for a given city
+    func fetchWeather(forCity city: String, completion: @escaping (WeatherData?, Error?) -> Void) {
+        // Implement the API call here
+        // On successful fetch, call completion with WeatherData
+        // On failure, call completion with Error
+    }
+
+    func saveLastSearchedCity(cityName: String) {
+        UserDefaults.standard.set(cityName, forKey: "lastSearchedCity")
+    }
+
+    func loadLastSearchedCity() -> String? {
+        return UserDefaults.standard.string(forKey: "lastSearchedCity")
+    }
+}
+
+// You'll need to define WeatherData based on the data you expect from the API
+struct WeatherData {
+    // Define properties like temperature, icon, etc.
+}
