8b6cd396be052bab6583034553c3ea95e51c3a52
Eric Palma <palmae95@gmail.com>
2023-12-12 07:29:06 -0500

    define pi config and endpoint

--- WeatherApp/WeatherApp/Services/WeatherAPI.swift
+++ WeatherApp/WeatherApp/Services/WeatherAPI.swift
@@ -6,3 +6,58 @@
 //
 
 import Foundation
+
+struct APIConfiguration {
+    let apiKey: String
+    let organization: String?
+    let baseURL: String
+    let scheme: String = "https"
+    
+    var headers: [String: String]? {
+        var headers = [String: String]()
+        headers["Authorization"] = "Bearer \(apiKey)"
+
+        if let organization = organization {
+            headers["OpenAI-Organization"] = organization
+        }
+        
+        headers["Content-Type"] = "application/json"
+        
+        return headers
+    }
+    
+    init(apiKey: String, organization: String? = nil, baseURL: String) {
+        self.apiKey = apiKey
+        self.organization = organization
+        self.baseURL = baseURL
+    }
+    
+}
+
+struct APIEndpoint {
+    enum APIEndpointPath: String {
+        case chatCompletions = "/v1/chat/completions"
+    }
+    
+    let endpointPath: APIEndpointPath
+    let bodyData: Codable?
+    let apiConfiguration: APIConfiguration
+    
+    func urlRequest() -> URLRequest? {
+        let endpoint = endpointFor(endpointPath)
+        var urlRequest = endpoint.urlRequest(configuration: apiConfiguration)
+        let encoder = JSONEncoder()
+        encoder.keyEncodingStrategy = .convertToSnakeCase
+        if let bodyData = bodyData, let data = try? encoder.encode(bodyData) {
+            urlRequest?.httpBody = data
+        }
+        return urlRequest
+    }
+    
+    private func endpointFor(_ path: APIEndpointPath) -> Endpoint {
+        switch path {
+        case .chatCompletions:
+            return Endpoint(path: path.rawValue, method: .POST)
+        }
+    }
+}
