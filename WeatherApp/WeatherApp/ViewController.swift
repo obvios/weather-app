0f8ed8630af76df2a65bdcfd2b929233f22e3703
Eric Palma <palmae95@gmail.com>
2023-12-11 19:58:38 -0500

    create xcode project

---
+++ WeatherApp/WeatherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Eric Palma on 12/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
