b2be87013a9d8149a3e02c309e0b847f678c80d5
Eric Palma <palmae95@gmail.com>
2023-12-12 09:48:00 -0500

    request weather using city name

--- WeatherApp/WeatherApp/WeatherScreen/WeatherViewModel.swift
+++ WeatherApp/WeatherApp/WeatherScreen/WeatherViewModel.swift
@@ -20,6 +20,17 @@ class WeatherViewModel {
         let api = WeatherAPI()
         return try await api.requestWeatherData(lat: lat, lon: lon)
     }
+    
+    // Reverse geocodes city name and uses coordinates to reqeuest weather data
+    func requestWeather(cityName: String) async throws -> WeatherData {
+        let api = WeatherAPI()
+        let coordinates = try await api.requestGeoCoordinates(cityName: cityName)
+        guard let cityCoordinates = coordinates.first else {
+            // could create view model specific errors instead or return null
+            throw NetworkError.invalidResponse
+        }
+        return try await api.requestWeatherData(lat: cityCoordinates.lat, lon: cityCoordinates.lon)
+    }
 
     func saveLastSearchedCity(cityName: String) {
         UserDefaults.standard.set(cityName, forKey: "lastSearchedCity")
