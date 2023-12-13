90a28d4dd66f2aa8ddc093aff0b97cec0c98e2e8
Eric Palma <palmae95@gmail.com>
2023-12-13 06:36:33 -0500

    make methods private

--- WeatherApp/WeatherApp/WeatherScreen/WeatherViewModel.swift
+++ WeatherApp/WeatherApp/WeatherScreen/WeatherViewModel.swift
@@ -9,13 +9,13 @@ import Foundation
 
 class WeatherViewModel {
 
-    func requestWeather(lat: Double, lon: Double) async throws -> WeatherData {
+    private func requestWeather(lat: Double, lon: Double) async throws -> WeatherData {
         let api = WeatherAPI()
         return try await api.requestWeatherData(lat: lat, lon: lon)
     }
     
     // Reverse geocodes city name and uses coordinates to reqeuest weather data
-    func requestWeather(cityName: String) async throws -> WeatherData {
+    private func requestWeather(cityName: String) async throws -> WeatherData {
         let api = WeatherAPI()
         let coordinates = try await api.requestGeoCoordinates(cityName: cityName)
         guard let cityCoordinates = coordinates.first else {
@@ -25,15 +25,15 @@ class WeatherViewModel {
         return try await api.requestWeatherData(lat: cityCoordinates.lat, lon: cityCoordinates.lon)
     }
 
-    func saveLastSearchedCity(cityName: String) {
+    private func saveLastSearchedCity(cityName: String) {
         UserDefaults.standard.set(cityName, forKey: "lastSearchedCity")
     }
 
-    func loadLastSearchedCity() -> String? {
+    private func loadLastSearchedCity() -> String? {
         return UserDefaults.standard.string(forKey: "lastSearchedCity")
     }
     
-    func requestIconData(iconName: String) async throws -> Data {
+    private func requestIconData(iconName: String) async throws -> Data {
         let api = WeatherIconsAPI()
         return try await api.requestIconData(iconName: iconName)
     }
