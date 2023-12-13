e825aaf366d3e3a1548412522519d57ddb826ad3
Eric Palma <palmae95@gmail.com>
2023-12-13 06:24:27 -0500

    icons api config

--- WeatherApp/WeatherApp/Services/Networking/WeatherIconsAPI.swift
+++ WeatherApp/WeatherApp/Services/Networking/WeatherIconsAPI.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+
+struct WeatherIconsAPI {
+    let apiConfiguration: APIConfiguration = {
+        // Normally would not hard code api key in code base
+        let apiKey = "testKey"
+        let baseURL = "openweathermap.org"
+        return APIConfiguration(apiKey: apiKey, baseURL: baseURL)
+    }()
+}
