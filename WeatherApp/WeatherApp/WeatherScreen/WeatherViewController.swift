7fc81b6295c34069813d643a5fd2ea2c70d7de52
Eric Palma <palmae95@gmail.com>
2023-12-13 07:10:09 -0500

    start view controller

--- WeatherApp/WeatherApp/WeatherScreen/WeatherViewController.swift
+++ WeatherApp/WeatherApp/WeatherScreen/WeatherViewController.swift
@@ -6,14 +6,24 @@
 //
 
 import UIKit
+import Combine
 
 class ViewController: UIViewController {
-
+    let viewModel = WeatherViewModel()
+    private var cancellables = Set<AnyCancellable>()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // build view hierarchy
+        // tell vm we loaded
     }
 
-
+    private func observeViewModel() {
+        viewModel.uiDataPublisher
+            .receive(on: DispatchQueue.main)
+            .sink { uiData in
+                // TODO: Update UI
+            }
+    }
 }
 
