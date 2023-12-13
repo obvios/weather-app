9dec8884c9b76419208fc52783ca054d2a3d64e3
Eric Palma <palmae95@gmail.com>
2023-12-13 07:39:48 -0500

    iterating on view

--- WeatherApp/WeatherApp/WeatherScreen/WeatherViewController.swift
+++ WeatherApp/WeatherApp/WeatherScreen/WeatherViewController.swift
@@ -15,13 +15,46 @@ class ViewController: UIViewController {
     // UI Components
     private let textField = UITextField()
     private let button = UIButton(type: .system)
-    private var labelsStackView = UIStackView()
     private var topStackView = UIStackView()
+    private var weatherInfoStackView = UIStackView()
+    private var weatherIcon = UIImageView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // build view hierarchy
-        // tell vm we loaded
+        setupViews()
+        // TODO: tell vm we loaded
+    }
+    
+    private func setupViews() {
+        // Configure Text Field
+        textField.borderStyle = .roundedRect
+        textField.placeholder = "Enter text"
+
+        // Configure Button
+        button.setTitle("Press Me", for: .normal)
+        // TODO: Add button action
+        
+        // Configure Top Stack View
+        topStackView.axis = .horizontal
+        topStackView.alignment = .center
+        topStackView.distribution = .fill
+        topStackView.spacing = 8
+        topStackView.addArrangedSubview(textField)
+        topStackView.addArrangedSubview(button)
+
+        // Configure Labels Stack View
+        weatherInfoStackView.axis = .vertical
+        weatherInfoStackView.alignment = .fill
+        weatherInfoStackView.distribution = .equalSpacing
+        weatherInfoStackView.spacing = 8
+
+        // Add sample labels
+        for i in 1...5 {
+            let label = UILabel()
+            label.text = "Label \(i)"
+            weatherInfoStackView.addArrangedSubview(label)
+        }
     }
 
     private func observeViewModel() {
