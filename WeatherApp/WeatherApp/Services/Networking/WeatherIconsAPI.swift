ce6a95de4650c8dc323aae536043f3f0342d2c65
Eric Palma <palmae95@gmail.com>
2023-12-13 08:36:54 -0500

    update icons api to prevent issues

--- WeatherApp/WeatherApp/Services/Networking/WeatherIconsAPI.swift
+++ WeatherApp/WeatherApp/Services/Networking/WeatherIconsAPI.swift
@@ -9,14 +9,12 @@ import Foundation
 
 struct WeatherIconsAPI {
     let apiConfiguration: APIConfiguration = {
-        // Normally would not hard code api key in code base
-        let apiKey = "testKey"
         let baseURL = "openweathermap.org"
-        return APIConfiguration(apiKey: apiKey, baseURL: baseURL)
+        return APIConfiguration(apiKey: nil, baseURL: baseURL)
     }()
     
     func requestIconData(iconName: String) async throws -> Data {
-        let path = "/img/wn/" + iconName + "@1x.png"
+        let path = "/img/wn/" + iconName + "@2x.png"
         let endpoint = Endpoint(path: path, method: .GET)
         guard let urlRequest = endpoint.urlRequest(configuration: apiConfiguration), let url = urlRequest.url else {
             throw APIError.unableToBuildRequest
