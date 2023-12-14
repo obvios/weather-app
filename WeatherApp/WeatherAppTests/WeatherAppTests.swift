bf98bebcf9ebb011cbcb0d22472b9ddf90cc6140
Eric Palma <palmae95@gmail.com>
2023-12-14 08:36:36 -0500

    add test

--- WeatherApp/WeatherAppTests/WeatherAppTests.swift
+++ WeatherApp/WeatherAppTests/WeatherAppTests.swift
@@ -9,14 +9,24 @@ import XCTest
 @testable import WeatherApp
 
 final class WeatherAppTests: XCTestCase {
-    func testWeatherEndpoint() {
+    func testEndpointWithAPIKey() {
         let config = APIConfiguration(apiKey: "testKey", baseURL: "api.openweathermap.org")
         let queryItems: [URLQueryItem] = [.init(name: "param1", value: "value1")]
-        let urlRequest = Endpoint(path: "/weather/", method: .GET, parameters: queryItems).urlRequest(configuration: config)!
+        let endpoint = Endpoint(path: "/weather/", method: .GET, parameters: queryItems)
+        let urlRequest = endpoint.urlRequest(configuration: config)!
         
         XCTAssertEqual(urlRequest.url!.scheme!, "https")
         XCTAssertEqual(urlRequest.url!.absoluteString, "https://api.openweathermap.org/weather/?param1=value1&appid=testKey")
         XCTAssertEqual(urlRequest.value(forHTTPHeaderField: "Content-Type"), "application/json")
     }
 
+    func testEndpointWithoutAPIKey() {
+        let config = APIConfiguration(apiKey: nil, baseURL: "api.openweathermap.org")
+        let endpoint = Endpoint(path: "/img/wn/10d@2x.png", method: .GET)
+        let urlRequest = endpoint.urlRequest(configuration: config)!
+        
+        XCTAssertEqual(urlRequest.url!.scheme!, "https")
+        XCTAssertEqual(urlRequest.url!.absoluteString, "https://api.openweathermap.org/img/wn/10d@2x.png")
+        XCTAssertEqual(urlRequest.value(forHTTPHeaderField: "Content-Type"), "application/json")
+    }
 }
