9dac5f691d6caedaf3ac8b0197ae7aa60d8c2a40
Eric Palma <palmae95@gmail.com>
2023-12-13 20:39:42 -0500

    add name property

--- WeatherApp/WeatherApp/Models/WeatherData.swift
+++ WeatherApp/WeatherApp/Models/WeatherData.swift
@@ -7,9 +7,17 @@
 
 import Foundation
 
+/*
+ Could have used more descriptive names for these
+ structs by using CodingKeys
+ */
 struct WeatherData: Decodable {
+    /// contains the weather conditions information
     let weather: [WeatherCondition]
+    /// contains the weather measurements
     let main: WeatherMeasurements
+    /// name of location
+    let name: String
 }
 
 struct WeatherCondition: Decodable {
