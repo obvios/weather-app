09e8f7fce71099e06a7b2e8ffecbe236e4d4a4d3
Eric Palma <palmae95@gmail.com>
2023-12-13 21:44:56 -0500

    comment

--- WeatherApp/WeatherApp/Models/WeatherData.swift
+++ WeatherApp/WeatherApp/Models/WeatherData.swift
@@ -8,8 +8,8 @@
 import Foundation
 
 /*
- Could have used more descriptive names for these
- structs by using CodingKeys
+ Could have used more descriptive names for the
+ properties of these structs by using CodingKeys
  */
 struct WeatherData: Decodable {
     /// contains the weather conditions information
