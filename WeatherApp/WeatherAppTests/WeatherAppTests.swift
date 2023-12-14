0c454cfce9cbf68bbd4119fdcf6241c4f5c3a118
Eric Palma <palmae95@gmail.com>
2023-12-14 08:32:13 -0500

    add test case

--- WeatherApp/WeatherAppTests/WeatherAppTests.swift
+++ WeatherApp/WeatherAppTests/WeatherAppTests.swift
@@ -9,28 +9,14 @@ import XCTest
 @testable import WeatherApp
 
 final class WeatherAppTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testWeatherEndpoint() {
+        let config = APIConfiguration(apiKey: "testKey", baseURL: "api.openweathermap.org")
+        let queryItems: [URLQueryItem] = [.init(name: "param1", value: "value1")]
+        let urlRequest = Endpoint(path: "/weather/", method: .GET, parameters: queryItems).urlRequest(configuration: config)!
+        
+        XCTAssertEqual(urlRequest.url!.scheme!, "https")
+        XCTAssertEqual(urlRequest.url!.absoluteString, "https://api.openweathermap.org/weather/?param1=value1&appid=testKey")
+        XCTAssertEqual(urlRequest.value(forHTTPHeaderField: "Content-Type"), "application/json")
     }
 
 }
