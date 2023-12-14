2364940bc6086a8ef29c7956a64f1648e26ded44
Eric Palma <palmae95@gmail.com>
2023-12-13 21:23:27 -0500

    refactor

--- WeatherApp/WeatherApp/WeatherScreen/WeatherViewController.swift
+++ WeatherApp/WeatherApp/WeatherScreen/WeatherViewController.swift
@@ -13,15 +13,29 @@ class WeatherViewController: UIViewController {
     private var cancellables = Set<AnyCancellable>()
     
     // UI Components
+    private let searchButton = UIButton(type: .system)
+    private var topStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.alignment = .center
+        stackView.distribution = .fill
+        stackView.spacing = 8
+        return stackView
+    }()
+    private var weatherInfoStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.alignment = .fill
+        stackView.distribution = .equalSpacing
+        stackView.spacing = 8
+        return stackView
+    }()
     private let searchTextField: UITextField = {
         let textField = UITextField()
         textField.borderStyle = .roundedRect
         textField.placeholder = "Enter city"
         return textField
     }()
-    private let searchButton = UIButton(type: .system)
-    private var topStackView = UIStackView()
-    private var weatherInfoStackView = UIStackView()
     private let weatherIcon: UIImageView = {
         let imageView = UIImageView()
         imageView.image = UIImage(named: "NoImage")
@@ -64,19 +78,11 @@ class WeatherViewController: UIViewController {
         searchButton.setTitle("Search", for: .normal)
         searchButton.addTarget(self, action: #selector(searchButtonClicked), for: .touchDown)
         
-        // Configure Top Stack View
-        topStackView.axis = .horizontal
-        topStackView.alignment = .center
-        topStackView.distribution = .fill
-        topStackView.spacing = 8
+        // Add search components to stack view
         topStackView.addArrangedSubview(searchTextField)
         topStackView.addArrangedSubview(searchButton)
 
-        // Configure Labels Stack View
-        weatherInfoStackView.axis = .vertical
-        weatherInfoStackView.alignment = .fill
-        weatherInfoStackView.distribution = .equalSpacing
-        weatherInfoStackView.spacing = 8
+        // Add weather info components to stack view
         weatherInfoStackView.addArrangedSubview(locationNameLabel)
         weatherInfoStackView.addArrangedSubview(primaryWeatherLabel)
         weatherInfoStackView.addArrangedSubview(weatherDescriptionLabel)
@@ -84,7 +90,6 @@ class WeatherViewController: UIViewController {
     }
     
     private func layoutViews() {
-        // Add stack views to the view
         view.translatesAutoresizingMaskIntoConstraints = false
         topStackView.translatesAutoresizingMaskIntoConstraints = false
         weatherIcon.translatesAutoresizingMaskIntoConstraints = false
@@ -119,14 +124,18 @@ class WeatherViewController: UIViewController {
         viewModel.uiDataPublisher
             .receive(on: DispatchQueue.main)
             .sink { [weak self] uiData in
-                self?.weatherIcon.image = UIImage(data: uiData.iconData)
-                // Instead of setting labels like this by concatenating string, would normally
-                // use a horizontal UIStack
-                self?.locationNameLabel.text = "Location: " + uiData.cityName
-                self?.primaryWeatherLabel.text = "Weather: " + uiData.weather
-                self?.weatherDescriptionLabel.text = "Description: " + uiData.weatherDescription
-                self?.temperatureLabel.text = "Temperature: " + String(uiData.temperature) + " Kelvin"
+                self?.updateUI(with: uiData)
             }.store(in: &cancellables)
+    }
+    
+    private func updateUI(with weatherData: WeatherScreenUIData) {
+        // Instead of setting labels like this by concatenating string, would normally
+        // use a horizontal UIStack
+        weatherIcon.image = UIImage(data: weatherData.iconData)
+        locationNameLabel.text = "Location: " + weatherData.cityName
+        primaryWeatherLabel.text = "Weather: " + weatherData.weather
+        weatherDescriptionLabel.text = "Description: " + weatherData.weatherDescription
+        temperatureLabel.text = "Temperature: " + String(weatherData.temperature) + " Kelvin"
     }
 }
 
